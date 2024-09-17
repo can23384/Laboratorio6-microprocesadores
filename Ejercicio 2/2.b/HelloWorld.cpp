7246327eb69fdcb177c79579e43ed14c9a303a97
can23384 <can23384@uvg.edu.gt>
2024-09-16 18:02:17 -0600

    Ejercicio 2.b

---
+++ Ejercicio 2/2.b/HelloWorld.cpp
@@ -0,0 +1,40 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_THREADS 10
+
+void *PrintHello(void *paramID) {
+    int *id;
+    id = (int *)paramID;
+    printf("Hello World! from thread #%d!\n", *id);
+    pthread_exit(NULL);
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t threadsID[NUM_THREADS];
+    int rc, t, param[NUM_THREADS];
+
+    for (t = 0; t < NUM_THREADS; t++) {
+        printf("In main: creating thread %d\n", t);
+        param[t] = t;
+        rc = pthread_create(&threadsID[t], NULL, PrintHello, (void *)&param[t]);
+        if (rc) {
+            printf("ERROR; return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
+
+    
+    for (t = 0; t < NUM_THREADS; t++) {
+        rc = pthread_join(threadsID[t], NULL);
+        if (rc) {
+            printf("ERROR; return code from pthread_join() is %d\n", rc);
+            exit(-1);
+        }
+    }
+
+    printf("All threads completed.\n");
+    return 0;
+}
